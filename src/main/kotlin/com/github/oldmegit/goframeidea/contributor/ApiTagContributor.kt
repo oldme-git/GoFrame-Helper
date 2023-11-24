9b03aa08e4ef12fb563f9682a949e9c9ea7b0440
oldme <tyyn1022@gmail.com>
2023-11-24 18:45:20 +0800

    api tag

--- src/main/kotlin/com/github/oldmegit/goframeidea/contributor/ApiTagContributor.kt
+++ src/main/kotlin/com/github/oldmegit/goframeidea/contributor/ApiTagContributor.kt
@@ -1,7 +1,8 @@
 package com.github.oldmegit.goframeidea.contributor
 
 import com.github.oldmegit.goframeidea.provider.ApiTagProvider
-import com.goide.GoParserDefinition
+import com.goide.GoTypes
+import com.goide.psi.GoTag
 import com.intellij.codeInsight.completion.CompletionContributor
 import com.intellij.codeInsight.completion.CompletionType
 import com.intellij.patterns.PlatformPatterns
@@ -10,7 +11,8 @@ class ApiTagContributor: CompletionContributor() {
     init {
         extend(
             CompletionType.BASIC,
-            PlatformPatterns.psiElement().withElementType(GoParserDefinition.Lazy.STRING_LITERALS),
+            PlatformPatterns.psiElement(GoTypes.RAW_STRING).withSuperParent(2, GoTag::class.java),
+//            PlatformPatterns.psiElement(GoTypes.STRING),
             ApiTagProvider()
         )
     }
--- src/main/kotlin/com/github/oldmegit/goframeidea/provider/ApiTagProvider.kt
+++ src/main/kotlin/com/github/oldmegit/goframeidea/provider/ApiTagProvider.kt
@@ -1,18 +1,16 @@
 package com.github.oldmegit.goframeidea.provider
 
-import com.github.oldmegit.goframeidea.utils.isApi
-import com.goide.psi.GoFile
+import com.goide.psi.GoAnonymousFieldDefinition
+import com.goide.psi.GoFieldDefinition
 import com.goide.psi.GoStructType
-import com.goide.psi.GoTag
 import com.intellij.codeInsight.completion.CompletionParameters
 import com.intellij.codeInsight.completion.CompletionProvider
 import com.intellij.codeInsight.completion.CompletionResultSet
 import com.intellij.codeInsight.lookup.LookupElementBuilder
-import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiFile
-import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.util.ProcessingContext
+import java.io.File
+import java.nio.file.Paths
 
 class ApiTagProvider: CompletionProvider<CompletionParameters>() {
     override fun addCompletions(
@@ -21,44 +19,71 @@ class ApiTagProvider: CompletionProvider<CompletionParameters>() {
         result: CompletionResultSet
     ) {
         val position = parameters.position
-
         val project = position.project
         val basePath = project.basePath
         val filePath = parameters.originalFile.virtualFile.path
 
-        // check file in api
-        if (!isApi(basePath.toString(), filePath)) {
+        // check if the file is in the api dir
+        if (!isApiFile(basePath.toString(), filePath)) {
             return
         }
 
-        // check position in tag
-        if (!checkTag(position)) {
+        // only struct name containing "Req" can need code completion
+        if (!isStructReq(position)) {
             return
         }
+
+        // get filed name
+        val filedName = getFieldName(position)
+        println(filedName)
 
         result.addElement(LookupElementBuilder.create("hello1"))
         result.addElement(LookupElementBuilder.create("hello2"))
     }
 
-    private fun checkTag(position: PsiElement): Boolean {
-        // check position in struct
-        val struct = PsiTreeUtil.findFirstParent(
-            position
-        ) { e: PsiElement? -> e is GoStructType } as GoStructType?
+    private fun getStructType(position: PsiElement): PsiElement? {
+        val structType = position.parent.parent.parent.parent
+        if (structType is GoStructType) {
+            return structType
+        }
+        return null
+    }
 
-        if (struct == null) {
+    private fun getFiledDefinition(position: PsiElement): PsiElement? {
+        val fieldDefinition = position.parent.parent.parent.firstChild
+        if (fieldDefinition is GoFieldDefinition || fieldDefinition is GoAnonymousFieldDefinition) {
+            return fieldDefinition
+        }
+        return null
+    }
+
+    private fun isStructReq(position: PsiElement): Boolean {
+        val structType = getStructType(position)
+        if (structType == null) {
             return false
         }
 
-        // check position in struct tag
-        val tag = PsiTreeUtil.findFirstParent(
-            position
-        ) { e: PsiElement? -> e is GoTag } as GoTag?
+        val structName = structType.prevSibling.prevSibling.text
+        return structName.takeLast(3) == "Req"
+    }
 
-        if (tag == null) {
-            return false
+    private fun getFieldName(position: PsiElement): String {
+        val name = getFiledDefinition(position)?.text
+        if (name != null) {
+            return name
         }
+        return ""
+    }
 
-        return true
+    // check if the file is in the api dir
+    private fun isApiFile(base: String, file: String): Boolean {
+        val basePath = Paths.get(base)
+        val filePath = Paths.get(file)
+        var absolute = basePath.relativize(filePath).toString()
+        if (File.separator == "\\") {
+            absolute = absolute.replace("\\", "/")
+        }
+        val absolutePaths = absolute.split("/")
+        return absolutePaths[0] == "api"
     }
 }
