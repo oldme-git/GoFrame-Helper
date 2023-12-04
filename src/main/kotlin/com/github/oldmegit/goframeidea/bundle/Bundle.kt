01f81f2e6f0a868ef90845477b740d23a5c4fb90
oldme <tyyn1022@gmail.com>
2023-12-04 15:53:58 +0800

    Bundle

---
+++ src/main/kotlin/com/github/oldmegit/goframeidea/bundle/Bundle.kt
@@ -0,0 +1,14 @@
+package com.github.oldmegit.goframeidea.bundle
+
+import com.intellij.DynamicBundle
+import java.util.function.Supplier
+
+object Bundle : DynamicBundle("message.GoFrameIdeaBundle") {
+    override fun getMessage(key: String, vararg params: Any?): String {
+        return super.getMessage(key, *params)
+    }
+
+    override fun getLazyMessage(key: String, vararg params: Any?): Supplier<String> {
+        return super.getLazyMessage(key, *params)
+    }
+}
