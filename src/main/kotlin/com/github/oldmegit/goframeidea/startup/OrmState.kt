80effaadecb52221e16b1c6c841c273e9d847b48
oldme <tyyn1022@gmail.com>
2023-12-04 16:05:58 +0800

    orm state

--- src/main/kotlin/com/github/oldmegit/goframeidea/startup/OrmState.kt
+++ src/main/kotlin/com/github/oldmegit/goframeidea/startup/OrmState.kt
@@ -1,4 +1,31 @@
 package com.github.oldmegit.goframeidea.startup
 
-class OrmState {
+import com.intellij.openapi.components.PersistentStateComponent
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.State
+import com.intellij.openapi.components.Storage
+import com.intellij.openapi.project.Project
+import com.intellij.util.xmlb.XmlSerializerUtil
+
+@Service(Service.Level.PROJECT)
+@State(
+    name = "com.github.oldmegit.goframeidea.ui.GoFrameIdeaOrm",
+    storages = [Storage("GoFrameIdeaOrm.xml")]
+)
+internal class OrmState : PersistentStateComponent<OrmState> {
+    var a: String? = null
+
+    override fun getState(): OrmState {
+        return this
+    }
+
+    override fun loadState(state: OrmState) {
+        XmlSerializerUtil.copyBean(state, this)
+    }
+
+    companion object {
+        fun getInstance(project: Project): OrmState {
+            return project.getService(OrmState::class.java)
+        }
+    }
 }
