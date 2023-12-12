bfca749ff6d17b9bebf39a3c38f32699ce3eceba
oldme <tyyn1022@gmail.com>
2023-12-12 17:50:16 +0800

    compatibility 222

--- src/main/kotlin/com/github/oldmegit/goframeidea/startup/StartUp.kt
+++ src/main/kotlin/com/github/oldmegit/goframeidea/startup/StartUp.kt
@@ -6,10 +6,10 @@ import com.intellij.openapi.progress.BackgroundTaskQueue
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.startup.ProjectActivity
+import com.intellij.openapi.startup.StartupActivity
 
-class StartUp : ProjectActivity {
-    override suspend fun execute(project: Project) {
+class StartUp : StartupActivity {
+    override fun runActivity(project: Project) {
         val task = BackgroundTaskQueue(project, Bundle.getMessage("name"))
         task.run(object : Task.Backgroundable(project, Bundle.getMessage("init")) {
             override fun run(indicator: ProgressIndicator) {
