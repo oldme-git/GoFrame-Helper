4d446b369aca7550bf6877381b397befd75ffdc6
oldme <tyyn1022@gmail.com>
2024-04-05 14:11:25 +0800

    BackgroundTaskQueue deprecated

--- src/main/kotlin/com/github/oldmegit/goframehelper/startup/StartUp.kt
+++ src/main/kotlin/com/github/oldmegit/goframehelper/startup/StartUp.kt
@@ -2,18 +2,24 @@ package com.github.oldmegit.goframehelper.startup
 
 import com.github.oldmegit.goframehelper.data.Bundle
 import com.github.oldmegit.goframehelper.gf.GfGoMod
-import com.intellij.openapi.progress.BackgroundTaskQueue
 import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
 
+
 class StartUp : StartupActivity {
     override fun runActivity(project: Project) {
-        val task = BackgroundTaskQueue(project, Bundle.getMessage("name"))
-        task.run(object : Task.Backgroundable(project, Bundle.getMessage("init")) {
+        val taskTitle = Bundle.getMessage("name")
+        ProgressManager.getInstance().run(object : Task.Backgroundable(project, taskTitle) {
             override fun run(indicator: ProgressIndicator) {
+                if (indicator.isCanceled) {
+                    return
+                }
+                indicator.text = Bundle.getMessage("init")
                 GfGoMod.reset(project)
+                indicator.fraction = 1.0
             }
         })
     }
